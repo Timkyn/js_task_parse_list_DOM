7690342dbe3ec2595ab990e95ab8a17657e48ed4
Tymofii Fastovetskyi <timoxa.sykunov2017@gmail.com>
2024-12-12 14:24:25 +0200

    create a dataBase

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+const listInfo = list.querySelectorAll('li');
+
+const dataBase = [];
+
+for (const li of listInfo) {
+  dataBase.push(li);
+}
+
+list.appendChild(dataBase[6]);
